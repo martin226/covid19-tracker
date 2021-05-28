0d3c6995298712cd05c9d2dfe5fe818a44fa0418
Martin Sit <46298951+martin-sit@users.noreply.github.com>
2021-05-28 15:08:37 -0400

    adjusted background colors

--- components/stats/Daily.vue
+++ components/stats/Daily.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="py-20 px-5 sm:px-32 grid grid-cols-1 lg:grid-cols-2 gap-x-16">
+    <div
+        class="
+            bg-gray-50
+            py-20
+            px-5
+            sm:px-32
+            grid grid-cols-1
+            lg:grid-cols-2
+            gap-x-16
+        "
+    >
         <div class="m-auto">
             <h1 class="mb-24 text-5xl xl:text-6xl 2xl:text-7xl font-extrabold">
                 Daily <span class="text-blue-800">COVID-19</span> Statistics
